a22822c2273e597f333964d0400605a29372ba90
AL08J11 <alberth.lop95@gmail.com>
2013-03-21 07:58:24 -0600

    version 1.1 de acciones

--- JS/acciones.js
+++ JS/acciones.js
@@ -29,5 +29,17 @@ $(document).ready(  function(e){
 			navigator.notification.alert("Desliz&oacute; a la derecha",function(){
 			},"Practica 1","Aceptar")
 		});
+		$("izquierda").swipeleft(function(){ navigator.notification.confirm('¿que decea hacer?',funtion(opcion){
+			switch(opcion){
+				case 1: 
+					navigator.notification.beep(1)
+				break
+				case 2 :
+					navigator.notification.vibrate(500)
+				break
+					
+			}//switch
+		},"practica1","beep,vibrar,cancelar");//confirm
+		});//function swipe
 	},false);
 });
